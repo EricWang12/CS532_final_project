42b3b2091012f92d14bf2d1d8ab0350498b2b945
Yuhua Chen <ychen664@wisc.edu>
2019-12-10 23:50:04 -0600

    Finished KNN tutorial with house made KNN.

--- KNN_tutorial.py
+++ KNN_tutorial.py
@@ -1,11 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+
 from math import sqrt
 
 
 # calculate the Euclidean distance between two vectors
 def euclidean_distance(row1, row2):
     distance = 0.0
-    for i in range(len(row1) - 1):
-        distance += (row1[i] - row2[i]) ** 2
+    for i in range(len(row1)):
+        # distance += (row1[i] - row2[i]) ** 2
+        distance = max(distance, abs(row1[i] - row2[i]))
     return sqrt(distance)
 
 
@@ -28,3 +34,49 @@ def predict_classification(train, test_row, num_neighbors):
     output_values = [row[-1] for row in neighbors]
     prediction = max(set(output_values), key=output_values.count)
     return prediction
+
+
+# kNN Algorithm
+def k_nearest_neighbors(train, test, num_neighbors):
+    predictions = list()
+    for row in test:
+        output = predict_classification(train, row, num_neighbors)
+        predictions.append(output)
+    return predictions
+
+
+# Adjust k, the number of neighbors here:
+n_neighbors = 7
+
+# import some data to play with
+iris = datasets.load_iris()
+
+# prepare data
+X = iris.data[:, :2]
+y = iris.target
+h = .05
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA','#00FFFF'])
+cmap_bold = ListedColormap(['#FF0000', '#008000','#0000FF'])
+
+# calculate min, max and limits
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# predict class using data and kNN classifier
+Z = k_nearest_neighbors(np.concatenate((X, y.reshape(-1,1)), axis=1), np.c_[xx.ravel(), yy.ravel()], n_neighbors)
+Z = np.asarray(Z)
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title("3-Class classification (k = %i)" % n_neighbors)
+plt.show()
