55096526ef3bcf79d8647b0a5d7b96180d585811
Eric Wang <zwang772@wisc.edu>
2019-12-11 20:43:28 -0600

    pygame

--- matlab/txt2mat.m
+++
@@ -1,7 +0,0 @@
-d = dir('*.txt');
-N=length(d);     % how many did we find?
-A=zeros(32,N);   % allocate for 24 points of data 1 column/file
-
-for k = 1 : length(d)   
-    M=dlmread(d(k).name)
-end
